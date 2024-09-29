7967de5d82deedb4cdf8a554fc9661cc1c24b64e
MinaSamir <MenaSamerCap@gmail.com>
2024-09-29 20:34:17 +0300

    traditional version of code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,32 @@
 import java.time.Clock
 import java.util.*
 
-class Logger(
-    private val includeDateTime: Boolean,
-    private val includeThreadName: Boolean,
-    private val includeUniqueId: Boolean,
-    private val clock: Clock
-) {
-    fun log(message: String) {
-        if (includeDateTime) print("[${clock.instant()}] ")
-        if (includeUniqueId) print("{${UUID.randomUUID()}} ")
+interface Logger {
+    fun log(message: String)
+}
+
+class ConsoleLogger : Logger {
+    override fun log(message: String) {
         print(message)
-        if (includeThreadName) print(" on ${Thread.currentThread().name} thread")
-        println()
     }
 }
 
+class UniqueIdLogger(private val logger: Logger) : Logger {
+    override fun log(message: String) =
+        logger.log("{${UUID.randomUUID()}} $message")
+}
+
+class ThreadNameLogger(private val logger: Logger) : Logger {
+    override fun log(message: String) =
+        logger.log("$message (on ${Thread.currentThread().name} thread)")
+}
+
+class DateTimeLogger(private val logger: Logger, private val clock: Clock = Clock.systemDefaultZone()) : Logger {
+    override fun log(message: String) =
+        logger.log("[${clock.instant()}] $message")
+}
 
 fun main() {
-    val logger = Logger(
-        includeDateTime = true,
-        includeThreadName = true,
-        includeUniqueId = false,
-        clock = Clock.systemDefaultZone()
-    )
+    val logger = UniqueIdLogger(ThreadNameLogger(DateTimeLogger(ConsoleLogger())))
     logger.log("Application initialized")
 }
